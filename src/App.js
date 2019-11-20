6d90827fcb286f2cdb707ba57816fc70c3f01a14
Tariq <tic084@gmail.com>
2019-11-20 12:50:14 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import DraftEditor from './DraftEditor';
+// import { EditorState, ContentState } from 'draft-js';
+import { EditorState, ContentState, convertToRaw, convertFromRaw } from 'draft-js';
+// import { draftToMarkdown } from 'markdown-draft-js';
+// import { mdToDraftjs, draftjsToMd } from 'draftjs-md-converter';
+import { Grid } from '@material-ui/core';
+import { createUseStyles } from 'react-jss';
+import clsx from 'clsx';
 
-function App() {
+const useStyles = createUseStyles(theme => ({
+  root: {
+    minHeight: '100vh',
+    padding: 100
+  },
+  panel: {
+    display: 'flex'
+  },
+  panel_left: {
+    borderRadius: [4, 0, 0, 4],
+    background: '#DCDBDC'
+  },
+  panel_right: {
+    borderRadius: [0, 4, 4, 0],
+    background: '#373737'
+  }
+}));
+
+// const noop = () => () => { };
+
+const App = () => {
+  const c = useStyles();
+  const [richEditorState, setRichEditorState] = useState(EditorState.createWithContent(ContentState.createFromText('')));
+  // const [markdownEditorState, setMarkdownEditorState] = useState(EditorState.createWithContent(ContentState.createFromText('')));
+  // const [onRichEditorChangeFn, setOnRichEditorChangeFn] = useState(noop);
+  // const [onMarkdownEditorChangeFn, setOnMarkdownEditorChangeFn] = useState(noop);
+
+  const onChange = useCallback((e) => {
+    // console.log(1);
+    setRichEditorState(e);
+    // const raw = convertToRaw(e.getCurrentContent());
+    // console.log(raw);
+    // const md = draftjsToMd(raw);
+    // const newMarkdownEditorState = EditorState.createWithContent(ContentState.createFromText(md));
+    // setMarkdownEditorState(newMarkdownEditorState);
+    // setOnRichEditorChangeFn(noop);
+    // setOnMarkdownEditorChangeFn(onMarkdownEditorChange);
+  }, [setRichEditorState]);
+
+  // const onMarkdownEditorChange = useCallback((e) => {
+  //   // console.log(2);
+  //   setMarkdownEditorState(e);
+  //   const md = e.getCurrentContent().getPlainText();
+  //   const rawData = mdToDraftjs(md);
+  //   const contentState = convertFromRaw(rawData);
+  //   const newRichEditorState = EditorState.createWithContent(contentState);
+  //   setRichEditorState(newRichEditorState);
+  //   setOnMarkdownEditorChangeFn(noop);
+  //   setOnRichEditorChangeFn(onRichEditorChange);
+  // }, []);
+
+  // useEffect(() => {
+  //   setOnRichEditorChangeFn(onRichEditorChange);
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container className={c.root}>
+      <Grid item xs={6} className={clsx(c.panel, c.panel_left)}>
+        <DraftEditor
+          draftId="rich"
+          editorState={richEditorState}
+          onChange={onChange}
+          showToolbar
+        />
+      </Grid>
+    </Grid>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
+import 'draft-js-static-toolbar-plugin/lib/plugin.css';
+import "prismjs/themes/prism.css"; // add prism.css to add highlights 
+// import 'draft-js/dist/Draft.css';
+// import 'draft-js-inline-toolbar-plugin/lib/plugin.css';
+// import 'draft-js-emoji-plugin/lib/plugin.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
